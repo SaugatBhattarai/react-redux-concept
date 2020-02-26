d70e449fccaeefa0f4e4fdbf36f9fde2155db7b2
Saugat Bhattarai <the.saggitorres@gmail.com>
2020-02-26 11:51:36 +0545

    added redux-logger middleware

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import reducer from "./reducers";
 import { buyCake, buyIcecream } from "./action";
+import { createLogger } from "redux-logger";
+
+const logger = createLogger({
+  // ...options
+});
+
 /**
  * Redux store
  * one store for the entire application
@@ -18,33 +24,36 @@ import { buyCake, buyIcecream } from "./action";
  */
 
 //1. Holds application state
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger));
 
 //2. Allows access to state via getState()
 console.log("Initial State", store.getState());
 
 //4. Registers listeners via subcribe(listener)
-const unsubscribe = store.subscribe(
-  () => console.log("State updated"),
-  store.getState()
-);
+// const unsubscribe = store.subscribe(
+//   () => console.log("State updated"),
+//   store.getState()
+// );
+
+//USE THIS WITH REDUX LOGGER -- don't need to console log
+const unsubscribe = store.subscribe(() => {});
 
 //3. Allows state to be updated via dispatch(action)
 //buyCake() function is action creaters
 store.dispatch(buyCake()); //state updated
-console.log("First cake Dispatch", store.getState());
+// console.log("First cake Dispatch", store.getState());
 
 store.dispatch(buyCake()); //state updated
-console.log("Second cake Dispatch", store.getState());
+// console.log("Second cake Dispatch", store.getState());
 
 store.dispatch(buyCake()); //state updated
-console.log("Third cake Dispatch", store.getState());
+// console.log("Third cake Dispatch", store.getState());
 
 store.dispatch(buyIcecream()); //state updated
-console.log("First icecream Dispatch", store.getState());
+// console.log("First icecream Dispatch", store.getState());
 
 store.dispatch(buyIcecream()); //state updated
-console.log("Second icecream Dispatch", store.getState());
+// console.log("Second icecream Dispatch", store.getState());
 
 // 5- Handles unregistering of listeners via the function returned
 // by subscribe (listener)
